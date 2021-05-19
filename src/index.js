425c644f6eacab9649571f66e0847bc89298c86b
Edy Segura <ednardo.segura@mundiale.com.br>
2021-05-18 22:31:06 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const PORT = process.env.PORT || 4000
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + "/index.html")
+})
+
+io.on('connection', socket => {
+    console.log('A user connected')
+    socket.on('disconnect', () => console.log('A user disconnect'))
+    socket.on('chat-message', msg => {
+        io.emit('chat-message', msg)
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('The server is running: http://localhost:' + PORT)
+})
